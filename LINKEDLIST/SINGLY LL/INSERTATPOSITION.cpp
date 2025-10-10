7eaa790dea9493533a4ce7dc58229295316daeb7
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2025-10-10 23:34:37 +0530

    NSERTATPOSITION

---
+++ LINKEDLIST/SINGLY LL/INSERTATPOSITION.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node * next;
+
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+};
+void insertatend(Node*& head , int value)
+{
+    Node * newNode = new Node(value);
+    Node * tail = NULL;
+    if(head== NULL)
+    {
+        head= newNode;
+        return;
+    }
+    Node*temp=head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next = newNode;
+    tail=newNode;
+}
+void insertatposition(Node*& head , int pos, int value)
+{
+    Node * newNode = new Node(value);
+    
+    
+    Node * temp = head;
+    int count =1;
+    while(temp!=NULL && count<pos-1)
+    {
+        temp = temp->next;
+        count++;
+    }
+    if(pos<=1 || head==NULL)
+    {
+        newNode->next=head;
+        head = newNode;
+    }
+    if(temp == NULL)
+    {
+     cout << "Position out of range!" << endl;
+        delete newNode;
+        return;
+    }
+    newNode->next = temp->next;
+    temp->next=newNode;
+}
+void printlist(Node*head)
+{
+    Node*temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL";
+    cout<<endl;
+}
+int main()
+{
+    Node* head = NULL;
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        int value;
+        cin>> value;
+        insertatend(head,value);
+    }
+    insertatposition(head ,10, 36);
+    printlist(head);
+}
