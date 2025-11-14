287977990991633046f7a798ef55c7a51dbd8eac
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2025-11-14 22:05:37 +0530

    Create reversearray.cpp

---
+++ Array/reversearray.cpp
@@ -0,0 +1,35 @@
+Given an array , reverse the  array , without using any extra space 
+
+Input :
+5
+1 2 3 4 5
+o/p
+5 4 3 2 1
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x ;
+        v.push_back(x);
+    }
+    int left=0 , right =n-1;
+
+    while(left<right)
+    {
+        swap(v[left],v[right]);
+        left++;
+        right--;
+    }
+
+    for(int x:v)
+        cout<<x<<" ";
+}
