6c6fe940dd0ad95f10e3629c8149170d6cb83dd4
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2025-11-14 13:50:38 +0530

    Update greaterelement .cpp

--- Array/greaterelement .cpp
+++ Array/greaterelement .cpp
@@ -32,3 +32,77 @@ int main()
     }
     cout<<count<<endl;
 }
+
+*/---2nd
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int>v;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    int count=1;
+    int maX=v[0];
+    int N=v.size();
+    for (int i=1; i<v.size(); i++)
+    {
+        
+            if(maX<v[i])
+            {    maX=v[i];
+                count=1;
+                
+              
+            }
+            else if(v[i]==maX)
+                count++;
+            
+       
+    }
+    int out= v.size()-count;
+    cout<<maX<<" "<<out<< " "<<N<<endl;
+}
+
+
+
+
+3rd #include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int>v;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    int count=0;
+    int maX=v[0];
+    for (int i=0; i<v.size(); i++)
+    {
+        if(maX<v[i])
+            {    maX=v[i];
+            
+            }
+       
+    }
+    for(int i=0; i<v.size(); i++)
+    {
+        if(maX!=v[i])
+            count++;
+    }
+    cout<<maX<<" "<<count<< endl;
+}
