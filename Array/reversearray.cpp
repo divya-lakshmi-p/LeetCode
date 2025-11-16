231fc36f93c97267650b0a4fb2852e409b4fd7fe
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2025-11-16 15:36:19 +0530

    Update reversearray.cpp

--- Array/reversearray.cpp
+++ Array/reversearray.cpp
@@ -33,3 +33,47 @@ int main()
     for(int x:v)
         cout<<x<<" ";
 }
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x ;
+        v.push_back(x);
+    }
+    for(int i=n-1; i<0; i--)
+    {
+        swap(v[i] , v[n-1-i]);
+    }
+   for(auto val:v)
+    cout<<val<<" ";
+}
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x ;
+        v.push_back(x);
+    }
+    for(int i=n-1; i>=0; i--)
+    {
+        cout<<v[i];
+    }
+}
