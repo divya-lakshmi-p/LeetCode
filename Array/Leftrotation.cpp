53355271e1a559847b928dcfc1760546093d4b84
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2025-11-19 10:38:38 +0530

    Create Leftrotation.cpp

---
+++ Array/Leftrotation.cpp
@@ -0,0 +1,64 @@
+Left rotation of array
+
+#include<bits/stdc++.h>
+using namespace std;
+void reversearray(vector<int> &v , int left , int right)
+{
+    while(left<right)
+    {swap(v[left],v[right]);
+     left++;
+     right--;}
+     
+
+}
+void rotatearr(vector<int>&v ,int k)
+{   int n = v.size();
+    
+    reversearray(v,0,k-1);
+    reversearray(v,k,n-1);
+    reversearray(v,0,n-1);
+
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int k;
+    cin>>k;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
+    rotatearr(v,k);
+
+    for(int val:v)
+      cout<<val<< " ";
+}
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int k;
+    cin>>k;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
+    vector<int>res(n);
+    //k=k%n;
+    for(int i=0; i<n; i++)
+    {
+       int position = (i-k+n)%n;
+       res[position] = v[i];
+    }
+    for(auto val:res)
+      cout<<val<<" ";
+}
