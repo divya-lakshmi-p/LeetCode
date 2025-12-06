007c3a84ffc4bc78cfd96a3308a742c63a4a863a
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2025-12-06 19:17:49 +0530

    Create COunt the  number of greatest strings.cpp

---
+++ Array/COunt the  number of greatest strings.cpp
@@ -0,0 +1,56 @@
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+Return the number of consistent strings in the array words.
+
+ 
+
+Example 1:
+
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+Output: 2
+Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+Example 2:
+
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+Output: 7
+Explanation: All strings are consistent.
+Example 3:
+
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+Output: 4
+Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+    ///insert all the allowed value into hashset
+        unordered_set<char>allowedset;
+
+        for(auto val:allowed)
+        {
+            allowedset.insert(val);
+        }
+
+        int count=0;
+        //taking string 
+        for(string & word:words){
+            bool isconsistent = true;
+//chacking srtings each charracted whether that is presented in hash set are not if its there we will increase the count else will break it 
+            for(char ch:word)
+            {
+                if(allowedset.find(ch)==allowedset.end())
+                {
+                    isconsistent=false;
+                    break;
+                }
+            }
+
+            if(isconsistent)
+            {
+                count++;
+            }
+        }
+      return count;  
+    }
+};
