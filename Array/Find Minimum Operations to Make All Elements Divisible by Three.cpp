1a0745ede08f95a01b692bb190c333f33958ac59
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2025-12-23 00:00:09 +0530

    Update Find Minimum Operations to Make All Elements Divisible by Three.cpp

--- Array/Find Minimum Operations to Make All Elements Divisible by Three.cpp
+++ Array/Find Minimum Operations to Make All Elements Divisible by Three.cpp
@@ -61,3 +61,32 @@ int main()
     }
     cout<<result<<endl;
 }
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int>v(n);
+    int count=0;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+        if(v[i]%3 !=0)
+            count++;
+    }
+
+    
+
+    cout<<count<<endl;
+
+
+}
