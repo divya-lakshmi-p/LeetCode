3535cee5480589bba22ad8fbf3fa7881b885d0b3
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-01-05 08:01:08 +0530

    Update move all zeros to right .cpp

--- Array /move all zeros to right .cpp
+++ Array /move all zeros to right .cpp
@@ -1,3 +1,26 @@
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -5,9 +28,7 @@ int main()
     int n;
     cin >> n;
     vector<int>v(n);
-    bool sortandrotate=true;
-    int k=0;
-   // unordered_map<int, int>mp;
+   
     for(int i=0; i<n; i++)
     {
         cin>>v[i];
@@ -29,3 +50,27 @@ int main()
 
     cout<<endl;
 }
+
+
+
+
+second way 
+
+
+
+
+        int nonzero =0;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[nonzero++]=nums[i];
+            }
+        }
+
+        for(int i=nonzero; i<nums.size(); i++)
+        {
+            nums[i]=0;
+        }
+        
