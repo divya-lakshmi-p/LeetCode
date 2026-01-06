631445ec8d32536f3938319842755a60efd22747
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-01-06 08:18:46 +0530

    Update Sum of all possible subbarray.cpp

--- Array /Sum of all possible subbarray.cpp
+++ Array /Sum of all possible subbarray.cpp
@@ -1,3 +1,6 @@
+
+TC:o(n^3)
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -22,6 +25,33 @@ int sum=0;
             }
             cout<<sum<<" "; 
         }
+        cout<<endl;
+    }
+cout<<endl;
+}
+
+
+TC:o(n^2)
+    #include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    
+    }
+int sum=0;
+    for(int i=0; i<n; i++)
+    {sum =0;
+        for(int j=i; j<=n; j++)
+        { sum+=v[j];
+            }
+        cout<<sum<<" "; 
         cout<<endl;
     }
 cout<<endl;
