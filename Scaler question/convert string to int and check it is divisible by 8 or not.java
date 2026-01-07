e3071669ad0c544006b880123531629568a22e35
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-01-07 09:01:04 +0530

    Update convert string to int and check it is divisible by 8 or not.java

--- Scaler question/convert string to int and check it is divisible by 8 or not.java
+++ Scaler question/convert string to int and check it is divisible by 8 or not.java
@@ -21,3 +21,25 @@ public class Solution {
        return (sum%8 ==0)? 1: 0;
     }
 }
+
+
+
+In C++
+    int solve(string A) {
+    int n = A.length();
+
+    // If the number has less than 3 digits, convert whole thing
+    if (n <= 3) {
+        int num = stoi(A);
+        return (num % 8 == 0) ? 1 : 0;
+    }
+
+    // Extract last 3 digits
+    int d1 = A[n - 3] - '0';
+    int d2 = A[n - 2] - '0';
+    int d3 = A[n - 1] - '0';
+
+    int lastThree = d1 * 100 + d2 * 10 + d3;
+
+    return (lastThree % 8 == 0) ? 1 : 0;
+}
