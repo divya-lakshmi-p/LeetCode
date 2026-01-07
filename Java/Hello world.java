204c8960f9852cbef0bdc625d71eac692465b377
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-01-07 23:10:25 +0530

    Create Hello world.java

---
+++ Java/Hello world.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+    public static void main(String [] args)
+    {
+        System.out.println("Hi Divya youu are going to be rich soon");
+    }
+}
+
+
+
+/*public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, DIVYA   welcome to learning java !");
+    }
+}*/
