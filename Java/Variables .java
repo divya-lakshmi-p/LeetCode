ade3f6c326199b6e3206d62561560bc8d7a1e052
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-01-08 23:12:44 +0530

    Create Variables .java

---
+++ Java/Variables .java
@@ -0,0 +1,24 @@
+class HelloWorld{
+    public static void main(String args[])
+    {
+        int age = 26;
+        String name = "DIVYA";
+        System.out.println(name);
+    }
+}
+
+
+
+Operators 
+  class HelloWorld{
+    public static void main(String args[])
+    {
+        int a=10 , b=2;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}  
