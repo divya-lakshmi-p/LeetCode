464f44a2a3a537c230c8e4bf7a346cdd2422f168
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-01-08 17:30:46 +0530

    Update Equilliburium index.cpp

--- Prefixsum/Equilliburium index.cpp
+++ Prefixsum/Equilliburium index.cpp
@@ -32,3 +32,33 @@ int main()
 cout<<endl;
 }
 
+
+
+
+
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>pref(n);
+         // pref[0] = nums[0];
+        for(int i=0; i<n; i++)
+        {  
+            (i==0)? pref[i]=nums[i]:pref[i]=pref[i-1]+nums[i];
+        }
+        // int total = pref[n - 1];
+
+        for(int i=0; i<n;i++)
+        {
+            int left =  (i==0?0:pref[i-1]);
+            int right = pref[n-1] -pref[i];
+
+            if(left == right)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
