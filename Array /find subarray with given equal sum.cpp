f4ae34f8216cf87ba1839e7f4d80d23568cfa5f1
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-01-08 21:23:07 +0530

    Create find subarray with given equal sum.cpp

---
+++ Array /find subarray with given equal sum.cpp
@@ -0,0 +1,59 @@
+Given a 0-indexed integer array nums, determine whether there exist two subarrays of length 2 with equal sum. Note that the two subarrays must begin at different indices.
+
+Return true if these subarrays exist, and false otherwise.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,4]
+Output: true
+Explanation: The subarrays with elements [4,2] and [2,4] have the same sum of 6.
+Example 2:
+
+Input: nums = [1,2,3,4,5]
+Output: false
+Explanation: No two subarrays of size 2 have the same sum.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: true
+Explanation: The subarrays [nums[0],nums[1]] and [nums[1],nums[2]] have the same sum of 0. 
+Note that even though the subarrays have the same content, the two subarrays are considered different because they are in different positions in the original array.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+     unordered_set<int>s;
+     for(int i=0;i<n-1;i++)
+     {
+        int sum = v[i]+v[i+1];
+
+        
+        if(s.count(sum))
+        {
+              cout<<"true";
+            return 0;
+
+        }
+          
+
+      s.insert(sum);
+     }
+     cout<<"false";
+     return 0;
+     
+}
