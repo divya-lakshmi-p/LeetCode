3312ac0398a97d9df3543ff06514907ce35c6978
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-01-09 20:40:27 +0530

    Update Variables .java

--- Java/Variables .java
+++ Java/Variables .java
@@ -22,3 +22,39 @@ Operators
         System.out.println(a%b);
     }
 }  
+
+
+Condtion 
+    class HelloWorld
+{
+    public static void main(String args[])
+    {
+        int age =26;
+
+        if(age>21)
+            System.out.println("You are eligible");
+        else
+            System.out.println("You are not eligible");
+    }
+}
+
+
+
+for and while loop
+
+class HelloWorld
+{
+    public static void main(String args[])
+    {   int i=1;
+        /*for(int i=0; i<n; i++)
+        {
+            System.out.println(i);
+        }*/
+       while(i<10)
+       {
+         System.out.println(i);
+         i++;
+       }
+
+    }
+}
