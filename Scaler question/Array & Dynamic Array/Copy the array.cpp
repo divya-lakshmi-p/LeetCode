bb0f40aad791b876ce743615963d51069caa8d4d
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-01-23 07:55:28 +0530

    Create Copy the array.cpp

---
+++ Scaler question/Array & Dynamic Array/Copy the array.cpp
@@ -0,0 +1,125 @@
+Problem Description
+
+You are given a constant array A and an integer B.
+
+You are required to return another array where outArr[i] = A[i] + B.
+
+
+
+Problem Constraints
+
+1 <= A.size() <= 10000
+
+1 <= A[i] <= 10000
+
+1 <= B <= 10000
+
+
+
+Input Format
+
+First argument is a constant array A.
+
+Second argument is an integer B.
+
+
+
+Output Format
+
+You have to return an integer array.
+
+
+
+Example Input
+
+Input 1:
+
+A = [1,2,3,2,1]
+B = 3
+Input 2:
+
+A = [1,1,10]
+B = 6
+
+
+Example Output
+
+Output 1:
+
+ [4,5,6,5,4] 
+Output 2:
+
+ [7,7,16] 
+
+
+Example Explanation
+
+Explanation 1:
+
+A[0] + 3 = 1 + 3 = 4
+A[1] + 3 = 2 + 3 = 5
+A[2] + 3 = 3 + 3 = 6
+A[3] + 3 = 2 + 3 = 5
+A[4] + 3 = 1 + 3 = 4
+
+
+
+
+
+
+vector<int> Solution::solve(const vector<int> &A, int B) {
+
+    vector<int>K;
+    for(int i=0; i<A.size(); i++)
+    {
+            K.push_back(A[i]+B);
+    }   
+
+    return K;
+
+}
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+  int n,b;;
+  cin>>n>>b;
+  vector<int>v(n);
+  for(int i=0; i<n; i++)
+  {
+    cin>>v[i];
+  }
+
+ /* vector<int>B=v;
+  int sum =0;
+  v.clear();
+  for(int i=0; i<n; i++)
+  {   sum=0;
+     sum+=B[i]+b;
+     v.push_back(sum);
+  }
+ 
+  for(auto val:v)
+  {
+    cout<<val<<" ";
+  }*/
+
+  for(int i=0; i<n; i++)
+  {
+    v.push_back(v[i]+b);
+  }
+
+  for(auto x:v)
+  {
+    cout<<x<<" ";
+  }
+  
+}
