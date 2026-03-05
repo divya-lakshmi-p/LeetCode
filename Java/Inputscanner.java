a71f7f7c6f584388b2438d1528b6f982acc310ab
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-03-05 07:02:49 +0530

    Create Inputscanner.java

---
+++ Java/Inputscanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main
+{
+    public static void main(String args[])
+  {
+       Scanner sc = new Scanner(System.in);
+
+       String s = sc.nextLine();
+       System.out.println("Entered string is "+s);
+
+       int x = sc.nextInt();
+       System.out.println("enter integer is "+x);
+       
+
+  }
+
+}
