ee1cdbc0017d190e458b429605598818dd6ab17d
divya-lakshmi-p <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-03-25 07:49:44 +0530

    Create COunt Setbits of the given number.cpp

---
+++ BIT MANIPLUATION/XOR/COunt Setbits of the given number.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int countsetbits(int n)
+{
+  int count =0;
+
+  while(n>0)
+  {
+    n= n&(n-1);
+    count++;
+  }
+  return count;
+}
+
+int main()
+{
+  int n ;
+  cin>>n;
+  cout<<countsetbits(n);
+}
