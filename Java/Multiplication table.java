31ecafc69a66856c129389671e74d265454051ac
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-04-16 21:47:36 +0530

    Create Multiplication table.java

---
+++ Java/Multiplication table.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+ class main
+{
+    public static void main(String args[])
+  {
+       Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        //
+        int sum =1;
+        for(int i=1; i<=x;i++)
+        {
+           // int num = sc.nextInt();
+             //sum *=i ;
+             System.out.println(x*i);
+            
+        }
+       
+       
+   System.out.println();
+  }
+
+}
