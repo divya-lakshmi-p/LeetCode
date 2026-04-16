17eec40e0546e6555ca17aee0178b92a2a3f5324
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-04-16 20:49:47 +0530

    Create even or odd.cpp

---
+++ Java/even or odd.cpp
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+ class main
+{
+    public static void main(String args[])
+  {
+       Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if(x%2 !=0)
+          System.out.println("odd");
+        else
+          System.out.println("even");
+       
+       
+
+  }
+
+}
