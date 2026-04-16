463cee976921de17896239cdf8b428eb82a5c719
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-04-16 20:54:13 +0530

    Create check which one is big.java

---
+++ Java/check which one is big.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+ class main
+{
+    public static void main(String args[])
+  {
+       Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        
+
+        if(x>y)
+          System.out.println("x is big "+x);
+        else
+          System.out.println("y is big "+y);
+       
+       
+
+  }
+
+}
