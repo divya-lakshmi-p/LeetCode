135929608c0c9613dac039554894a050d4411a81
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-04-16 21:30:30 +0530

    Create Print n numbers.java

---
+++ Java/Print n numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+ class main
+{
+    public static void main(String args[])
+  {
+       Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        
+        for(int i=1; i<=x;i++)
+        {
+           // int num = sc.nextInt();
+
+            System.out.print(i+" ");
+        }
+       
+       
+   System.out.println();
+  }
+
+}
