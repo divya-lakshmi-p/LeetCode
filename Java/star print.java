4d436b8da7086fbf9a2479e6814fef64a1da1d17
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-04-16 22:33:49 +0530

    Create star print.java

---
+++ Java/star print.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+ class main
+{
+    public static void main(String args[])
+  {
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //int count=0;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<=i;j++)
+            {
+                 System.out.print("*");
+            }
+            System.out.println();
+        }
+      
+    }
+       
+   
+  }
+
