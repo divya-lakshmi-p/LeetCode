543993b6f713b418c8f5a18d0b9793b461d563ce
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-04-16 22:48:21 +0530

    Update star print.java

--- Java/star print.java
+++ Java/star print.java
@@ -21,3 +21,29 @@ import java.util.Scanner;
    
   }
 
+
+REVERSE PATTERN
+
+import java.util.Scanner;
+
+ class main
+{
+    public static void main(String args[])
+  {
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //int count=0;
+        for(int i=n; i>0; i--)
+        {
+            for(int j=0; j<i;j++)
+            {
+                 System.out.print("*");
+            }
+            System.out.println();
+        } 
+      
+    }
+       
+   
+  }
+
