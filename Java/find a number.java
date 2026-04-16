7f3959db4e601349fcd7c5f1b18abb3da5ede594
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-04-16 20:43:32 +0530

    Create find a number.java

---
+++ Java/find a number.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+ class main
+{
+    public static void main(String args[])
+  {
+       Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if(x>0)
+          System.out.println("Positive");
+        else if(x<0)
+          System.out.println("Negative");
+        else
+          System.out.println("Zero");
+       
+       
+
+  }
+
+}
+
+
+1️⃣ Positive / Negative / Zero
+
+👉 Input a number
+👉 Print:
+
+Positive
+Negative
+Zero
