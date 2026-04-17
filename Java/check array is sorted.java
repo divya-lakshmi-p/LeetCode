7ec725634ff5ab8078f9850190f499851dfb24a1
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-04-17 20:30:27 +0530

    Create check array is sorted.java

---
+++ Java/check array is sorted.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+ class main
+{
+    public static void main(String args[])
+  {
+       
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int [] arr = new int[n];
+    boolean issort = true;
+    
+    for(int i=0; i<n; i++)
+    {
+        arr[i]=sc.nextInt();
+
+    }
+
+    for(int i=0; i<n-1; i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            issort = false;
+            break;
+        }
+    }
+        if(issort)
+            System.out.println("Array is sorted");
+        else
+            System.out.println("Array is not sorted");
+
+        
+        
+    }
+
+    
+    }
+        
+    
+       
+   
+
+
+  
