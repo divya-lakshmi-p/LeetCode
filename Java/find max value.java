751b7fcd45b4f3a914c122f7b498eae9c0679a50
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-04-17 17:56:16 +0530

    Create find max value.java

---
+++ Java/find max value.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+ class main
+{
+    public static void main(String args[])
+  {
+       
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int [] arr = new int[n];
+    int sum =Integer.MIN_VALUE;
+    for(int i=0; i<n; i++)
+    {
+        arr[i]=sc.nextInt();
+        if(sum<arr[i])
+            sum = arr[i];
+        
+    }
+
+    System.out.println(sum);
+    }
+       
+   
+  }
+
+  
