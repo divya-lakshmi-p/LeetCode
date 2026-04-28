f0a924cc4acf9591f85983e28a138143ed11be43
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-04-28 21:05:28 +0530

    Create count frequency of the given number.java

---
+++ Java/count frequency of the given number.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+import java.util.HashSet;
+
+
+class main
+{
+    public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+
+         int n = sc.nextInt();
+
+         int [] arr = new int [n];
+         int k = sc.nextInt();
+          
+         for(int i=0; i<n; i++)
+         {
+            arr[i]=sc.nextInt();
+            
+         }
+System.out.println((lastoccurence(arr,k)-firstoccurence(arr ,k))+1);
+} 
+
+
+static int lastoccurence(int [] arr , int k)
+{
+
+   int low =0 ;
+   int high = arr.length-1;
+   int ans =-1;
+
+   while(low<=high)
+   {
+      int mid = low +(high-low)/2;
+
+      if(arr[mid]==k)
+      {
+            ans = mid;
+            low = mid+1;
+      }
+      else if(arr[mid]>k)
+         high = mid-1;
+      else
+         low = mid+1;
+   }
+   return ans;
+}
+
+static int firstoccurence(int [] arr , int k)
+{
+
+   int low =0 ;
+   int high = arr.length-1;
+   int anss =-1;
+
+   while(low<=high)
+   {
+      int mid = low +(high-low)/2;
+
+      if(arr[mid]==k)
+      {
+            anss = mid;
+            high = mid-1;
+      }
+      else if(arr[mid]>k)
+         high = mid-1;
+      else
+         low = mid+1;
+   }
+   return anss;
+}
+ 
+    
+} 
+                
+
+
+        
+
+
+
+    
+
+
+
+
+
+
+
+
+
