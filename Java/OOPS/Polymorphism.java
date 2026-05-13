c61c475a550e583a21ea531ae95dd5de065850b1
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-05-13 09:01:15 +0530

    Update Polymorphism.java

--- Java/OOPS/Polymorphism.java
+++ Java/OOPS/Polymorphism.java
@@ -18,3 +18,36 @@ public class main{
         m.add(10,20);
     }
 }
+
+
+2.run time polymorphism 
+
+
+    
+class Math
+{
+    void add(int a , int b)
+    {
+            System.out.println(a+b);
+    }
+}
+
+class add extends Math
+{
+    void add(int a , int b)
+    {
+        System.out.println(a+b);
+    }
+}
+
+
+public class main{
+    public static void main(String args[])
+    {
+        Math m = new add();
+
+        m.add(8,2);
+
+        
+    }
+}
