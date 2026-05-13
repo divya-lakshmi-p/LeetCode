ee93b12a432cf7e42aa8965b6f7bd556f63764b9
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-05-13 21:12:44 +0530

    Update selfjoin.sql

--- SQL/JOIN/selfjoin.sql
+++ SQL/JOIN/selfjoin.sql
@@ -30,3 +30,73 @@ from employees e
 left join employees m
 on e.manager_id = m.id
 where m.name = 'Ram';
+
+
+
+
+1.Count employees under each manager
+
+select m.name as manager,
+count(e.id) as total_employees
+from employees e
+left join employees m
+on e.manager_id = m.id
+group by m.name;
+
+2.find manager with highest number of employees
+select m.name as manager,
+count(e.id) as total_employees
+from employees e
+left join employees m
+on e.manager_id = m.id
+group by m.name
+order by total_employees desc
+limit 1;
+
+3.Show managers who manage more than 1 employee
+
+select m.name as manager,
+count(e.id) as total_employees
+from employees e
+left join employees m 
+on e.manager_id = m.id
+group by m.name 
+having total_employees >1;
+
+4.Show managers with exactly 2 employees
+
+select m.name as manager,
+count(e.id) as total_employees
+from employees e
+left join employees m
+on e.manager_id = m.id;
+group by m.name
+having total_employees =2;
+
+5.Show managers with lest than 2 employees
+
+select m.name as manager,
+count(e.id) as total_employees
+from employees e
+left join employees m 
+on e.manager_id = m.id
+group by m.name 
+having total_employees <2 ;
+
+6.Find total employees under Ram
+
+select m.name as manager,
+count(e.id) as total_employees
+from employees e
+left join employees m 
+on e.manager_id = m.id
+where m.name = 'Ram'
+group by m.name ;
+
+7.Show employees working under Ravi
+
+select e.name as Employees,
+from employees e
+left join employees m 
+on e.manager_id = m.id
+where m.name = 'Ravi';
